f34468d1242e346d82043b7d95c5c710fec40500
Martin Broman <martinbroman90@gmail.com>
2025-01-17 03:54:49 +0100

    working with the project
    setting upp everything.

--- events/models.py
+++ events/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="events")
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
