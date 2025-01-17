f2dc9a4f9c683b5adc6c5b484cdb739f51c3ffd3
Martin Broman <martinbroman90@gmail.com>
2025-01-17 16:59:12 +0100

    changed back to a older commit

--- calendar_app/urls.py
+++ calendar_app/urls.py
@@ -1,22 +1,21 @@
-"""
-URL configuration for calendar_app project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from events.views import EventViewSet
 
+# Skapa en router för API:et
+router = DefaultRouter()
+router.register(r'events', EventViewSet)
+
+# Lägg till routen i URL-konfigurationen
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),  # Här kopplar vi in API:et
 ]
+from rest_framework import viewsets
+from .models import Event
+from .serializers import EventSerializer
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
